024ae7460d1422d505abbda5bd1a7945fed88a72
Frank Denis <github@pureftpd.org>
2016-11-07 21:21:46 +0100

    0.1.2

--- src/privdrop.rs
+++ src/privdrop.rs
@@ -1,61 +1,89 @@
-
 use errors::*;
 use libc;
 use nix::unistd;
+use std::ffi::CString;
 use std::path::{Path, PathBuf};
 
-#[derive(Default)]
+/// PrivDrop structure
+///
+/// # Example
+/// ```
+/// PrivDrop::default().chroot("/var/empty").user("_appuser").apply().unwrap();
+/// ```
+#[derive(Default, Clone, Debug)]
 pub struct PrivDrop {
-    chroot_dir: Option<PathBuf>,
+    chroot: Option<PathBuf>,
     user: Option<String>,
     group: Option<String>,
 }
 
 impl PrivDrop {
-    pub fn new() -> Self {
-        PrivDrop::default()
-    }
-
-    pub fn chroot_dir<T: AsRef<Path>>(mut self, path: T) -> Self {
-        self.chroot_dir = Some(path.as_ref().to_owned());
+    /// chroot() to a specific directory before switching to a non-root user
+    pub fn chroot<T: AsRef<Path>>(mut self, path: T) -> Self {
+        self.chroot = Some(path.as_ref().to_owned());
         self
     }
 
+    /// Set the name of a user to switch to
     pub fn user<T: AsRef<str>>(mut self, user: T) -> Self {
         self.user = Some(user.as_ref().to_owned());
         self
     }
 
-    fn do_preload(&self) -> Result<(), PrivDropError> {
+    /// Set a group name to switch to, if different from the primary group of the user
+    pub fn group<T: AsRef<str>>(mut self, group: T) -> Self {
+        self.group = Some(group.as_ref().to_owned());
+        self
+    }
+
+    /// Apply the changes
+    pub fn apply(self) -> Result<(), PrivDropError> {
+        try!(try!(try!(self.do_preload()).do_chroot()).do_userchange());
+        Ok(())
+    }
+
+    fn do_preload(self) -> Result<Self, PrivDropError> {
         unsafe {
             libc::strerror(1);
-            libc::setlocale(libc::LC_CTYPE, "C".as_ptr() as *const i8);
-            libc::setlocale(libc::LC_COLLATE, "C".as_ptr() as *const i8);
+            libc::setlocale(libc::LC_CTYPE, CString::new("C").unwrap().as_ptr());
+            libc::setlocale(libc::LC_COLLATE, CString::new("C").unwrap().as_ptr());
             let mut now: libc::time_t = 0;
             libc::time(&mut now);
             libc::localtime(&now);
         }
-        Ok(())
+        Ok(self)
     }
 
-    fn do_chroot(&self) -> Result<(), PrivDropError> {
-        if let Some(ref chroot_dir) = self.chroot_dir {
-            try!(unistd::chdir(chroot_dir));
-            try!(unistd::chroot(chroot_dir));
+    fn do_chroot(mut self) -> Result<Self, PrivDropError> {
+        if let Some(chroot) = self.chroot.take() {
+            try!(unistd::chdir(&chroot));
+            try!(unistd::chroot(&chroot));
             try!(unistd::chdir("/"))
         }
-        Ok(())
+        Ok(self)
     }
 
-    fn do_userchange(&self) -> Result<(), PrivDropError> {
-        if let Some(ref user) = self.user {
-            let pwent = unsafe { libc::getpwnam(user.as_ptr() as *const i8) };
+    fn do_userchange(mut self) -> Result<Self, PrivDropError> {
+        if let Some(user) = self.user.take() {
+            let pwent = unsafe {
+                libc::getpwnam(try!(CString::new(user).map_err(|_| {
+                        PrivDropError::from((ErrorKind::SysError,
+                                             "Unable to access the system user database"))
+                    }))
+                    .as_ptr())
+            };
             if pwent.is_null() {
                 return Err(PrivDropError::from((ErrorKind::SysError, "User not found")));
             }
             let (uid, gid) = (unsafe { *pwent }.pw_uid, unsafe { *pwent }.pw_gid);
-            let gid = if let Some(ref group) = self.group {
-                let grent = unsafe { libc::getgrnam(group.as_ptr() as *const i8) };
+            let gid = if let Some(group) = self.group.take() {
+                let grent = unsafe {
+                    libc::getgrnam(try!(CString::new(group).map_err(|_| {
+                            PrivDropError::from((ErrorKind::SysError,
+                                                 "Unable to access the system group database"))
+                        }))
+                        .as_ptr())
+                };
                 if grent.is_null() {
                     return Err(PrivDropError::from((ErrorKind::SysError, "Group not found")));
                 }
@@ -63,16 +91,13 @@ impl PrivDrop {
             } else {
                 gid
             };
+            if unsafe { libc::setgroups(1, &gid) } != 0 {
+                return Err(PrivDropError::from((ErrorKind::SysError,
+                                                "Unable to revoke supplementary groups")));
+            }
             try!(unistd::setgid(gid));
             try!(unistd::setuid(uid));
         }
-        Ok(())
-    }
-
-    pub fn apply(self) -> Result<(), PrivDropError> {
-        try!(self.do_preload());
-        try!(self.do_chroot());
-        try!(self.do_userchange());
-        Ok(())
+        Ok(self)
     }
 }
